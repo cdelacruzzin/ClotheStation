6df9dd0b8987cd2e18841d39c9569b5d2627bfa1
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-21 21:10:55 -0400

    fix formatting of resolvers, typeDefs, auth.js with prettier

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const secret = 'ilovesushi';
-const expiration = '2h';
+const secret = "ilovesushi";
+const expiration = "2h";
 
 module.exports = {
-    authMiddleware: function ({ req }) {
-        let token = req.body.token || req.query.token || req.headers.authorization;
-    
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
     if (req.headers.authorization) {
-        token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-        return req;
+      return req;
     }
 
     try {
-        const { data } = jwt.verify(token, secret, { maxAge: expiration });
-        req.user = data;
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
     } catch {
-        console.log('Invalid token');
+      console.log("Invalid token");
     }
 
     return req;
-},
+  },
 
-signToken: function ({ email, username, _id }) {
+  signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-},
+  },
 };
