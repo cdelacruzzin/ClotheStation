ff89535778542c07655c47a7686f6f8029cac4ca
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-21 21:54:00 -0400

    add comments to auth.js and mutations.js in the client

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,16 +7,19 @@ class AuthService {
 
   loggedIn() {
     const token = this.getToken();
+    // return true if token not expired
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
   isTokenExpired(token) {
+    // decode token to get expiration set by server
     const decoded = decode(token);
+    // token is expired and returns true if expiration is less than current time(in seconds)
     if (decoded.exp < Date.now() / 1000) {
       localStorage.removeItem("_id_token");
       return true;
     }
-
+    // return false if token hasn't passed the expiration time
     return false;
   }
 
@@ -24,11 +27,13 @@ class AuthService {
     return localStorage.getItem("id_token");
   }
 
+//   set id token in localstrage if logged in
   login(idToken) {
     localStorage.setItem("id_token", idToken);
     window.location.assign("/");
   }
 
+//   after loggin out remove id token
   logout() {
     localStorage.removeItem("id_token");
     window.location.reload();
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,10 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
+// add mutation for login, accepting email and password
   mutation login($email: String!, $password: String) {
     login(email: $email, password: $password) {
+        // access token and logged in user
       token
       user {
         _id
@@ -13,8 +15,10 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
+// add mutation to add user with new username, email, password
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+        // add token and new user
       token
       user {
         _id
