a7a53e7dff879b1cdcd4128828b93c2f2572c505
Koro999 <chris.dang93@hotmail.com>
2023-09-25 19:20:43 -0400

    updated typeDefs. Added comments, category and Product types. Added remove and add product mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,31 +1,55 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-# user that takes id, username, email, password and products in an array
- type User {
+  # user that takes id, username, email, password and products in an array
+  type User {
     _id: ID
     username: String
     email: String
     password: String
     products: [Product]!
- }
+  }
 
-# Auth type to handle returning data from a profile creating or user login
+  type Product {
+    _id: ID!
+    name: String!
+    price: Float!
+    description: String!
+    category: [String!]!
+    comment: [Comment!]!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]! # A list of products in this category
+  }
+
+  #timestamp needs to be set to current time
+  type Comment {
+    id: ID!
+    username: String!
+    text: String!
+    timestamp: String!
+  }
+
+  # Auth type to handle returning data from a profile creating or user login
   type Auth {
     token: ID!
     user: User
   }
 
-
-# query to look for current user
+  # query to look for current user
   type Query {
     me: User
   }
-  
-# add new user and login user
+
+  # add new user and login user
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password!): Auth
+    login(email: String!, password: String!): Auth
+    saveProduct(Product: ProductData!): User
+    removeProduct(ProductId: String!:) User
   }
 `;
 
