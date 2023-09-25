f1a1896f3916c8f219a8dc87cb67363799a72dbc
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-25 14:47:27 -0400

    initiating models adding user

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Comment = require('./Commentr');
+const Order = require('./Order');
+
+
+module.exports = { User, Product, Category, Comment, Order };
