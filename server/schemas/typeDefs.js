4ea9bc32faf3f5f095b434bbefd1105c4da5e1ef
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-25 14:05:34 -0400

    bug fix gql syntaxt changed // to # for comments

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-// user that takes id, username, email, password and products in an array
+# user that takes id, username, email, password and products in an array
  type User {
     _id: ID
     username: String
@@ -10,21 +10,21 @@ const typeDefs = gql`
     products: [Product]!
  }
 
-//  Auth type to handle returning data from a profile creating or user login
+# Auth type to handle returning data from a profile creating or user login
   type Auth {
     token: ID!
     user: User
   }
 
 
-//   query to look for current user
+# query to look for current user
   type Query {
     me: User
   }
   
-//   add new user and login user
+# add new user and login user
   type Mutation {
-    addUser(username: !String, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password!): Auth
   }
 `;
