217a0f4171b03b7ea7f5352ac2d3338f901d5a7f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-27 18:28:31 -0400

    imported MUI to style the navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,21 +2,29 @@ import React from "react";
 import Auth from '../../utils/auth';
 import { Link } from "react-router-dom";
 
+import Button from '@mui/material/Button';
+import { Box } from "@mui/system";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-const Navbar = ()=>{
+
+
+const Navbar = () => {
 
 
     return (
         <header>
-            <h1>
-                <Link to="/" className="home-link">
-                    <span></span>
-                    URBAN SK8
-                </Link>
-            </h1>
-        </header>
+
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <Tabs centered>
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                </Tabs>
+            </Box>
+
+
+        </header >
     )
-
-
 }
 export default Navbar;
