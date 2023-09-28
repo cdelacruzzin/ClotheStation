5274bea0411820e45da2d80375cb073f5f7c75e1
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-27 20:01:04 -0400

    add stripe to the resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,9 @@
 // import authentificationerror , usermodel and signToken
 const { AuthentificationError } = require("apollo-server-express");
-const { User, Category, Comment, Product } = require("../models");
+const { User, Category, Comment, Product, Cart } = require("../models");
 const { signToken } = require("../utils/auth");
 const uuid = require('uuid');
-const stripe = require('stripe')('sk')
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 // Example function to generate a unique comment ID (you would implement this)
 function generateUniqueCommentId() {
@@ -25,8 +25,49 @@ const resolvers = {
     allProducts: async () => {
       return await Product.find({});
     },
-  },
+    order: async (parent, { _id }, context ) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.products',
+          populate: 'category',
+        });
 
+        return user.orders.id(_id);
+      }
+
+      throw new AuthentificationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      await Cart.create({ items: args.items.map(({ _id }) => _id) });
+      const line_items = [];
+
+      for (const product of args.items) {
+        line_items.push({
+          price_data: {
+            currency: 'cad',
+            product_data: {
+              name: product.name,
+              description: product.description,
+              images: [`${url}/images/${product.image}`],
+            },
+            unit_amount: product.price * 100,
+          },
+          quantity: product.quantity,
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`;
+        cancel_url: `${url}/`,
+      });
+
+      return { session: session.id };
+    },
+  },
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
       try {
