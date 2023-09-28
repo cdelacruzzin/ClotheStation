9c31b503c2d0b4682f9bf1dd92f7844c500ba121
Koro999 <chris.dang93@hotmail.com>
2023-09-27 20:25:36 -0400

    small comment added

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -7,28 +7,58 @@ const { productData } = require("./productData");
 
 db.once("open", async () => {
   try {
-    // Reset the database
+    //reset the database
     await User.deleteMany({});
     await Category.deleteMany({});
     await Product.deleteMany({});
 
     const user = await User.insertMany(userData);
     const category = await Category.insertMany(categoryData);
+    const product = await Product.create(productData);
 
-    // Create or update products in the database
-    const savedProducts = await Product.create(productData);
 
-    for (const product of savedProducts) {
-      const categoryIds = product.categories || [];
-      const matchingCategories = categoryData.filter((cat) =>
-        categoryIds.includes(cat._id.toString())
-      );
+    //this is wrong or unnecessary 
+    productData.forEach(async(product) => {
+        // Store the array of category IDs for the product
+        const categoryIds = product.categories;
+      
+        // Initialize an array to store matching categories
+        const matchingCategories = [];
+        
+        //this logic here defines the ability to check for multiple categories
+        // Iterate through each category ID in the product
+        categoryIds.forEach((categoryId) => {
+          // Find the category object that matches the category ID
+          const category = categoryData.find((category) => category._id == categoryId);
+          // If a matching category is found, push it to the matchingCategories array
+          if (category) {
+            matchingCategories.push(category);
+          }
+        });
+        
+        // Now, matchingCategories contains all categories associated with the product
+        console.log('Matching Categories:', matchingCategories);
+      
+        // You can then loop through matchingCategories and update the category.products array
+        matchingCategories.forEach((category) => {
+          category.products.push(product._id);
+        });
 
-      for (const category of matchingCategories) {
-        category.products.push(product._id);
-        await category.save();
-      }
-    }
+        // Create or update products in the database
+        const savedProducts = Product.create(productData);
+
+        console.log(savedProducts)
+
+        matchingCategories.forEach(async (category) => {
+            // Update the category.products array in memory
+            category.products.push(savedProducts._id);
+            // Save the updated category to the database
+            await category.save();
+        });
+      
+        console.log('Product ID:', product._id);
+      });
+      
 
     console.table("Users seeded!");
     console.table("Categories seeded!");
