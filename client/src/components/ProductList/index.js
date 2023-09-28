453053b601eec1dde4f7de9c7c6da2cb8fab780f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-27 22:01:02 -0400

    finished dispatch

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,17 +2,21 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../../utils/globalState";
 import { QUERY_ALL_PRODUCTS } from '../../utils/queries';
+import {UPDATE_PRODUCTS} from '../../utils/actions';
+
 function ProductList() {
 
     const [state, dispatch] = useStoreContext();
     const { loading, data: productData } = useQuery(QUERY_ALL_PRODUCTS);
 
+    console.log(productData);
     useEffect(() => {
         if (productData) {
             dispatch({
-                
-            })
-        }
+                type: UPDATE_PRODUCTS,
+                products: productData.allProducts
+            });
+        };
 
     })
 
