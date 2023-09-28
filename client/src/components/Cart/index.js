2395c7f4f4cf9758eaa286568c15a5682f4d1473
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-27 20:24:31 -0400

    add cart component and import necessary functions, queries, helpers and dependencies

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import { useStoreContext } from '../../utils/globalState';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../..utils/actions';
+
+// use stripePromise for testing
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+const Cart = () => {
+    const [state, dispatch] = useStoreContext();
+}
