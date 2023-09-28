e337b1f80d5d3fa29a21a76cb9633043b06b6b67
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-27 20:54:09 -0400

    created reducer and actions to update the global state categories

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,5 +1,8 @@
 import { useReducer } from "react";
 //TODO: import actions.
+import {
+    UPDATE_CATEGORIES
+} from './actions';
 
 
 /**this reducer function is the main function to manage the global state based on dispatch actions.
@@ -12,24 +15,31 @@ import { useReducer } from "react";
  *  - it may contain 2 parameters:
  *      *type: this property is the modification type you want to apply.
  *      * an additional property: additional data to determine how the state should change*/
-export const reducer = (state, action) =>{
+export const reducer = (state, action) => {
+    // console.log(state)
+    // console.log(action)
+    switch (action.type) {
 
-    switch(action.type) {
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
+            }
         default:
             return state;
     }
 }
 
-export function useProductReducer(initialState){
-    
-  // The `useReducer` function accepts a reducer function and an initial state as its arguments.
-  /** 1) Reducer function:
-   * a function that takes the current state and an action object, and returns the new state
-   *  2) initialState:
-   * the starting state for the app component. it's the state before any actions have been dispatched
-   * 
-   * the useReducer hook returns an array of two elements:
-   *    -the current state(based on all the actions that have been dispatched)
-   *    - the dispatch function, which is used to dispatch actions to modify the state*/
+export function useProductReducer(initialState) {
+
+    // The `useReducer` function accepts a reducer function and an initial state as its arguments.
+    /** 1) Reducer function:
+     * a function that takes the current state and an action object, and returns the new state
+     *  2) initialState:
+     * the starting state for the app component. it's the state before any actions have been dispatched
+     * 
+     * the useReducer hook returns an array of two elements:
+     *    -the current state(based on all the actions that have been dispatched)
+     *    - the dispatch function, which is used to dispatch actions to modify the state*/
     return useReducer(reducer, initialState);
 }
