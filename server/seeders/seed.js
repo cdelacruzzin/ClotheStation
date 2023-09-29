04116e3c85e984751000e698f50f622854ca90ef
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-09-29 15:16:06 -0400

    working on category-product relations

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -14,13 +14,39 @@ db.once("open", async () => {
 
     //seed information
     await User.insertMany(userData);
-    await Category.insertMany(categoryData);
-    await Product.create(productData);
-      
-
     console.table("Users seeded!");
-    console.table("Categories seeded!");
-    console.table("Products seeded!");
+
+    // Seed categories
+    await Category.insertMany(categoryData);
+    console.log("Categories seeded!");
+
+    // Seed products
+    await Product.create(productData);
+    console.log("Products seeded!");
+
+    // Fetch all categories and products from the database after insertion
+    const allCategories = await Category.find({});
+    const allProducts = await Product.find({});
+    
+    // Associate products with categories and vice versa
+    for (const product of allProducts) {
+        for (const category of allCategories) {
+            if (product.name.includes(category.name)) {
+                console.log(`${category.name}: ${category._id}  ${product.name}: ${product._id}`);
+                
+                // Update product with category id
+                await Product.findByIdAndUpdate(product._id, { $push: { categories: category._id } });
+                
+                // Update category with product id
+                await Category.findByIdAndUpdate(category._id, { $push: { products: product._id } });
+            }
+        }
+    }
+    
+    const newProd = await Product.find();
+
+    console.log(newProd)
+    console.log("Categories updated with products!");
     process.exit(0);
   } catch (err) {
     console.error(err);
