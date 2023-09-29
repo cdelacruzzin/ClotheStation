a647d30e5709a0b6cbfa412471f1f5318df013ae
PythonFabi <fabianmelone.fw@gmail.com>
2023-09-28 23:45:40 -0400

    create skeleton for productItem

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// alll passed props will be passed into this function from the spread props object
+// No need to define individually
 function DeleteBtn(props) {
     return (
         <span {...props} role="button" tabIndex="0">
