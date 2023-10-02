aee0fca2e9e1fbb2e5023abe40ed26a8725df6e1
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-10-02 11:39:08 -0400

    add to cart functionality works

--- client/src/pages/SingleItem.jsx
+++ client/src/pages/SingleItem.jsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { QUERY_PRODUCT } from '../utils/queries';
 import { useQuery } from "@apollo/client";
-import { SET_CURRENT_PRODUCT } from '../utils/actions';
+import { SET_CURRENT_PRODUCT, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from '../utils/actions';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+
+import { idbPromise } from "../utils/helpers";
 
 // import "./css/SingleItem.scss";
 
@@ -15,9 +17,10 @@ function SingleProduct() {
     const { loading, data: queryProduct } = useQuery(QUERY_PRODUCT, {
         variables: useParams()
     });
+    const { id } = useParams();
     const [state, dispatch] = useStoreContext();
-
     const { products, cart, selectedProduct } = state;
+    const [currentProduct, setCurrentProduct] = useState({});
 
     useEffect(() => {
         if (queryProduct) {
@@ -26,12 +29,62 @@ function SingleProduct() {
                 selectedProduct: queryProduct.product,
             })
         }
-    }, [dispatch, products, queryProduct, selectedProduct])
+    }, [dispatch, products, queryProduct, selectedProduct]);
 
 
-    function addToCart(){
+    useEffect(() => {
+        // already in global store
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        }         // // retrieved from server
+        else if (queryProduct) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: queryProduct.products,
+            });
 
+            //   queryProduct.products.forEach((product) => {
+            //     idbPromise('products', 'put', product);
+            //   });
+        }
+        // get cache from idb
+        else if (!loading) {
+            //   idbPromise('products', 'get').then((indexedProducts) => {
+            //     dispatch({
+            //       type: UPDATE_PRODUCTS,
+            //       products: indexedProducts,
+            //     });
+            //   });
+        }
+        // console.log(currentProduct)
+
+        // console.log(queryProduct)
+
+    }, [products, queryProduct, loading, dispatch, id]);
+
+    function addToCart() {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
+        console.log(currentProduct)
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
+            //   idbPromise('cart', 'put', {
+            //     ...itemInCart,
+            //     purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            //   });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                product: { ...currentProduct, purchaseQuantity: 1 },
+            });
+            //   idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+        }
     }
+
+    console.log(state)
     return (
         <>
             {selectedProduct ? (
@@ -55,8 +108,8 @@ function SingleProduct() {
                     </p>
                     <div>
                         <button
-                        onClick={addToCart}
-                        ><FontAwesomeIcon icon={faCartPlus} className="cart-icon"/></button>
+                            onClick={addToCart}
+                        ><FontAwesomeIcon icon={faCartPlus} className="cart-icon" /></button>
                     </div>
                 </div>
             ) : null}
