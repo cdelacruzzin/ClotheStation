91e19d7e577860fe48b672b0fa24c906e8c978a4
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-10-06 10:35:30 -0400

    fix styling of productcarousell and add skatedecks to the seeds

--- server/seeders/productData.js
+++ server/seeders/productData.js
@@ -83,13 +83,70 @@ const productData = [
     imageSource: "https://www.sk8clothing.com/cdn/shop/files/NBNumeric440TrailShoeBlackGreen_480x.jpg?v=1692199979"
   },
   {
-    name: "Decks High-Top Black",
-    price: 94.99,
-    description: "High-top black decks, perfect for casual outings",
+    name: "Decks Element Calavera Black Gold Deck 8.5",
+    price: 85.99,
+    description: "Celebrating the day of Dead tradition",
     categories: [],
     comment: [],
-    imageSource: "https://www.sk8clothing.com/cdn/shop/files/NBNumeric440TrailShoeBlackGreen_480x.jpg?v=1692199979"
-  }
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/elementCALAVERABlackgoldSkateboardDeck85_1800x1800.jpg?v=1683553670"
+  },
+  {
+    name: "Decks HUF x Avengers Oh Snap Deck, 8.25",
+    price: 120.99,
+    description: "Celebrating 60 years of earths mightiest",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/HUFxMarvelAvengersOhSnapSkateboardDeck825_1800x1800.webp?v=1692879753"
+  },
+  {
+    name: "Decks Primitive x Call of Duty Rodriguez Ghost Deck 8.125",
+    price: 135.99,
+    description: "Get ready for the battlefield like a ghost",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/PrimitivexCallofDutyRodriguezGhostDeck8125_1800x1800.webp?v=1692879780"
+  },
+  {
+    name: "Decks Grizzly Marshmellow Complete 7.75",
+    price: 110.99,
+    description: "Prebuilt grizzly skateboard",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/collections/new-arrivals-skateboards/products/grizzly-marshmellow-complete-7-75"
+  },
+  {
+    name: "Decks Alien Workshop Inflatable Intel Deck 8.125",
+    price: 120.99,
+    description: "Some Alien Fun",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/alienworkshopInflatableIntelskateboarddeckPurple8125_1800x1800.webp?v=1693333342"
+  },
+  {
+    name: "Decks Primitive Rodriguez Holofoil Eagle Deck 8.25",
+    price: 124.99,
+    description: "Primitive Gold Foil Deck Limited Edition",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/PrimitiveRodriguezHolofoilEagleDeck825_1800x1800.webp?v=1694190461"
+  },
+  {
+    name: "Decks Santa Cruz Classic Dot Complete 8.0",
+    price: 120.99,
+    description: "Santa Cruz Beginner Friendly Skateboard",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/files/santacruzclassicdotcomplete_1800x1800.jpg?v=1696083267"
+  },
+  {
+    name: "Decks Santa Cruz Stranger Things Season 3 Deck 8.5",
+    price: 115.99,
+    description: "The Stranger Things Deck from the Starcourt mall",
+    categories: [],
+    comment: [],
+    imageSource: "https://www.sk8clothing.com/cdn/shop/products/Santa-Cruz-Stranger-Things-Season-3-Deck-8-5_1800x1800.jpg?v=1671235574"
+  },
+  
 ]
 
 module.exports = { productData };
